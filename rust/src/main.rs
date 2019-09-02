212dbcbdf0c24908bae4a1b00e05e15bf9bc0e9b
Franz König <>
2019-09-02 18:08:45 +0200

    rust: http GET working

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,19 @@
+use std::io::{stdout, Write};
+
+extern crate curl;
+use curl::easy::Easy;
 
 
 fn main() {
-    print!("testKF")
+    print!("testKF\n");
+
+    let mut easy = Easy::new();
+    easy.url("https://chat.qed-verein.de/").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}", easy.response_code().unwrap());
 }
