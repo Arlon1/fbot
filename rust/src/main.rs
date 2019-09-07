3917f119d173eba67ec2a80031e7563c4b6c68be
Franz König <>
2019-09-07 20:49:59 +0200

    added a lib for the qedchat

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,36 @@
 use std::fs;
 
 extern crate serde;
-//use serde::{Serialize, Deserialize};
-
 mod config;
+
+extern crate qedchat;
+use qedchat::{BotTag, Client, Post, SendPost};
+
+
+fn botloop(channel: String) {
+    let client = Client::new("FranzBots", "").unwrap();
+    let mut channel = client.listen_to_channel(channel, -10).unwrap();
+
+    loop {
+        let post = channel.receive().unwrap();
+        println!("[{}] [{}] {}", post.date, post.post.name, post.post.message);
+        if post.post.bottag == BotTag::Human && post.post.message.starts_with("!ping") {
+            channel
+                .send(&SendPost {
+                    post: Post {
+                        name: "fbot".to_owned(),
+                        message: "pong".to_owned(),
+                        channel: post.post.channel,
+                        bottag: BotTag::Bot,
+                        delay: post.id + 1,
+                    },
+                    publicid: false,
+                })
+                .unwrap();
+        }
+    }
+}
+
 
 fn main() {
     // TODO: command line parsing
@@ -13,8 +40,9 @@ fn main() {
     let contents = fs::read_to_string("fbot_r.toml").expect("fehler");
 
     let _botconf: config::BotConfig = toml::from_str(&contents).unwrap();
-    for element in _botconf.bots.channel {
-        println!("channel ist {}", element);
-        // TODO spawn a client for every channel
+    for channel in _botconf.bots.channel {
+        println!("channel ist {}", channel);
+
+        /* async */ botloop(channel);
     }
 }
