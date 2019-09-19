051b1e1763c6ed72d7293d2b4f1ba8998b1f751a
Franz <>
2019-09-19 21:54:20 +0200

    outlined a struct/impl ("class") that handles one channel

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,46 +3,23 @@ use std::fs;
 extern crate serde;
 mod config;
 
-extern crate qedchat;
-use qedchat::{BotTag, Client, Post, SendPost};
-
-
-fn botloop(channel: String) {
-    let client = Client::new("FranzBots", "").unwrap();
-    let mut channel = client.listen_to_channel(channel, -10).unwrap();
-
-    loop {
-        let post = channel.receive().unwrap();
-        println!("[{}] [{}] {}", post.date, post.post.name, post.post.message);
-        if post.post.bottag == BotTag::Human && post.post.message.starts_with("!ping") {
-            channel
-                .send(&SendPost {
-                    post: Post {
-                        name: "fbot".to_owned(),
-                        message: "pong".to_owned(),
-                        channel: post.post.channel,
-                        bottag: BotTag::Bot,
-                        delay: post.id + 1,
-                    },
-                    publicid: false,
-                })
-                .unwrap();
-        }
-    }
-}
-
+mod channel_handler;
+use channel_handler::ChannelHandler;
 
 fn main() {
     // TODO: command line parsing
     // e.g. "-c config.toml"
 
     // reading the config file into a string
-    let contents = fs::read_to_string("fbot_r.toml").expect("fehler");
+    let contents = fs::read_to_string("fbot.toml").expect("fehler");
 
     let _botconf: config::BotConfig = toml::from_str(&contents).unwrap();
     for channel in _botconf.bots.channel {
-        println!("channel ist {}", channel);
+        println!("starting fbot for channel \"{}\"", channel);
 
-        /* async */ botloop(channel);
+        let handler = ChannelHandler {channel_name: "fbot".to_string()};
+        // TODO: async
+        handler.botloop();
+        
     }
 }
