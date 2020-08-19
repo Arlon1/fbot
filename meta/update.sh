4fec7be3c89c5e22c14c34eb4cf6667738ff2e93
Franz <>
2020-08-19 23:38:46 +0200

    meta/update.sh: typo

--- meta/update.sh
+++ meta/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-gh-pages_url='https://arlon1.github.io/fbot/rusty_rita'
-wget ${gh-pages_url}/rusty_rita -O /opt/fbot/rusty_rita
-wget ${gh-pages_url}/fbot.service -O /etc/systemd/system/fbot.service
+gh_pages_url='https://arlon1.github.io/fbot'
+wget ${gh_pages_url}/rusty_rita -O /opt/fbot/rusty_rita
+wget ${gh_pages_url}/fbot.service -O /etc/systemd/system/fbot.service
 systemctl restart fbot.service
